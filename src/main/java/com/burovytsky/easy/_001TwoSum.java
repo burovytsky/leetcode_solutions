c818890984a5a54b68ad6aa4367d67d7ef613b12
burovytsky <esfero1505@gmail.com>
2020-12-21 19:09:44 +0200

    #001 Two sum

---
+++ src/main/java/com/burovytsky/easy/_001TwoSum.java
@@ -0,0 +1,25 @@
+package com.burovytsky.easy;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * <p>
+ * Created by burovytsky on 21/12/2020.
+ */
+public class _001TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
